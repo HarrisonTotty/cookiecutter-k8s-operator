68b87b8c06efb99043b1c37293292189efa37753
Harrison Totty <harrisongtotty@gmail.com>
2021-07-29 09:39:50 -0500

    Build out utilities library

--- {{cookiecutter.project_name}}/src/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.module_name}}/__init__.py
@@ -9,8 +9,11 @@ __VERSION__ = "0.1.0"
 import kopf
 import kubernetes
 import logging
+import os
 import pkgutil
 import yaml
+
+from . import utils
 
 
 @kopf.on.cleanup()
@@ -21,24 +24,38 @@ def cleanup(logger, **kwargs):
     logging.debug('Shutting down...')
 
 
+@kopf.on.login()
+def login(**kwargs):
+    '''
+    Handles cluster authentication.
+    '''
+    logging.debug('Authenticating with kubernetes...')
+    return kopf.login_with_service_account(**kwargs) or kopf.login_with_kubeconfig(**kwargs)
+
+
 @kopf.on.startup()
-def startup(logger, **kwargs):
+def startup(settings: kopf.OperatorSettings, **kwargs):
     '''
     Provides additional events to occur upon startup of the operator.
     '''
-    logging.debug('Loading kubernetes client configuration...')
+    logging.debug('Loading initial kubernetes cluster configuration...')
     try:
-        kubernetes.config.load_kube_config()
+        if os.getenv('KUBECONFIG', '') or os.path.exists(os.path.expanduser('~/.kube/config')):
+            logging.debug('Loading local cluster configuration...')
+            kubernetes.config.load_kube_config()
+        else:
+            logging.debug('Loading cluster configuration from service account...')
+            kubernetes.config.load_incluster_config()
     except Exception as e:
-        raise Exception(f'Unable to handle operator startup - unable to load k8s config - {e}')
+        raise Exception(f'Unable to load initial kubernetes cluster configuration - {e}')
     logging.debug('Loading custom resource definition(s)...')
     try:
-        data = yaml.safe_load_all(pkgutil.get_data(__name__, 'crds.yaml'))
+        crds = list(yaml.safe_load_all(pkgutil.get_data(__name__, 'crds.yaml')))
     except Exception as e:
         raise Exception(f'Unable to handle operator startup - unable to load CRDs - {e}')
     logging.debug('Creating custom resource definition(s)...')
     try:
-        client = kubernetes.client.ApiClient()
-        kubernetes.utils.create_from_dict(client, data)
+        for crd in crds:
+            utils.apply_resource(crd)
     except Exception as e:
         raise Exception(f'Unable to handle operator startup - unable to create CRDs - {e}')
--- {{cookiecutter.project_name}}/src/{{cookiecutter.module_name}}/utils.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.module_name}}/utils.py
@@ -4,3 +4,96 @@ Common Utility Functions
 
 import kopf
 import kubernetes
+import logging
+
+
+def apply_resource(body: dict):
+    '''
+    Creates or updates the specified kubernetes resource, as if you had run
+    `kubectl apply -f`.
+    '''
+    kind = body['kind']
+    name = body['metadata']['name']
+    namespace = body['metadata'].get('namespace')
+    api = get_api(body['apiVersion'], kind)
+    rstr = f'[{api.kind}] {namespace}/{name}'
+    logging.debug(f'Checking if "{rstr}" already exists...')
+    existing = get_resource(name=name, namespace=namespace, api=api)
+    if existing is None:
+        logging.info(f'Creating "{rstr}"...')
+        apply_method = api.create
+    else:
+        logging.info(f'Patching "{rstr}"...')
+        apply_method = api.patch
+    try:
+        if namespace is None:
+            response = apply_method(body=body, name=name)
+        else:
+            response = apply_method(body=body, name=name, namespace=namespace)
+    except Exception as e:
+        logging.error(f'Unable to apply resource "{rstr}" - {e}')
+        raise e
+    return response
+
+
+def delete_resource(name: str, namespace: str = None, body: dict = None, api = None, api_version: str = None, kind: str = None):
+    '''
+    Deletes the specified resource. Note that either `api` or `api_version` and
+    `kind` must be specified. The body of the resource may be specified instead
+    of `name` and `namespace`.
+    '''
+    if body is None:
+        _api_version = api_version
+        _kind        = kind
+        _name        = name
+        _namespace   = namespace
+    else:
+        _api_version = body['apiVersion']
+        _kind        = body['kind']
+        _name        = body['metadata']['name']
+        _namespace   = body['metadata'].get('namespace')
+    if api is None:
+        api = get_api(_api_version, _kind)
+    rstr = f'[{api.kind}] {_namespace}/{_name}'
+    logging.info(f'Deleting "{rstr}"...')
+    try:
+        if _namespace is None:
+            api.delete(body={}, name=_name)
+        else:
+            api.delete(body={}, name=_name, namespace=_namespace)
+    except Exception as e:
+        logging.error('Unable to delete "{rstr}" - {e}')
+        raise e
+
+
+def get_api(api_version: str, kind: str):
+    '''
+    Returns a dynamic API given the specified `api_version` and `kind`.
+    '''
+    client = kubernetes.dynamic.DynamicClient(kubernetes.client.ApiClient())
+    return client.resources.get(api_version=api_version, kind=kind)
+
+
+def get_resource(name: str, namespace: str = None, api = None, api_version: str = None, kind: str = None) -> dict:
+    '''
+    Gets the body of the specified resource, returning `None` if the resource
+    can't be found. Note that either `api` and `api_version` and `kind` must be
+    specified.
+    '''
+    if api is None:
+        api = get_api(api_version, kind)
+    rstr = f'[{api.kind}] {namespace}/{name}'
+    existing = None
+    logging.debug(f'Getting resource "{rstr}"...')
+    try:
+        if namespace is None:
+            existing = api.get(name=name).to_dict()
+        else:
+            existing = api.get(name=name, namespace=namespace).to_dict()
+    except kubernetes.client.ApiException as e:
+        if e.reason == 'Not Found':
+            logging.debug(f'Resource "{rstr}" does not exist.')
+            pass
+        else:
+            logging.debug(f'Unable to get resource "{rstr}" - {e}')
+    return existing
